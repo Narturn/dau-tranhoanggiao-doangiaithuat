87f94ec44008e5992a2500eddb65cd129c965202
Narturn <qtgiao@gmail.com>
2026-04-19 11:05:36 +0700

    update

---
+++ 101-SymmetricTree.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isSymmetric(self, root):
+        def mirror(a, b):
+            if not a and not b:
+                return True
+            if not a or not b:
+                return False
+            if a.val != b.val:
+                return False
+            
+            return(
+                mirror(a.left, b.right)
+                and
+                mirror(a.right, b.left)
+            )
+        
+        return mirror(root.left, root.right)
